2d2b002472df0687d0fb802392e7f28daf8121f3
carynova <carychan317@gmail.com>
2017-04-03 18:59:35 +0800

    problem 0003

---
+++ 0003-longest-substring-without-repeating-characters.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    var hashMap = {}
+    var startPos = 0
+    var maxLen = 0
+
+    // triverse the string
+    for (var i = 0, len = s.length; i < len; i++) {
+        var char = s[i]
+
+        if (!hashMap[char]) {
+            // new character
+            hashMap[char] = true
+        } else {
+            // get rid of the prefix one by one
+            while(1) {
+                if (s[startPos] === char) {
+                    startPos++
+                    break
+                }
+
+                hashMap[s[startPos]] = false
+                startPos++
+            }
+        }
+
+        // update max length
+        maxLen = Math.max(maxLen, i - startPos + 1)
+    }
+
+    return maxLen
+};
