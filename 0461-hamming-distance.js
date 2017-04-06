8abd929595267e12f7ce6e6cc276bfe1d9febf18
carynova <carychan317@gmail.com>
2017-04-06 17:41:17 +0800

    problem 0461 hmmm.. water

---
+++ 0461-hamming-distance.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} x
+ * @param {number} y
+ * @return {number}
+ */
+var hammingDistance = function(x, y) {
+    var tmp = x ^ y
+    var result = 0
+    for (var i = 0; i < 32; i++) {
+        result += (tmp & 1 ? 1 : 0)
+        tmp >>= 1
+    }
+    return result
+};
