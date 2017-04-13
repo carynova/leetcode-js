3d09f72346380d5b76d573073bd4d88c413852e5
carynova <carychan317@gmail.com>
2017-04-13 14:15:26 +0800

    0412 underscore range

--- 0412-fizz-buzz.js
+++ 0412-fizz-buzz.js
@@ -1,8 +1,24 @@
+/* global _ */
+
 /**
  * @param {number} n
  * @return {string[]}
  */
 var fizzBuzz = n => {
+  return _.range(1, n + 1).map(num => {
+    let tmpStr = ''
+    if (num % 3 === 0) {
+      tmpStr += 'Fizz'
+    }
+    if (num % 5 === 0) {
+      tmpStr += 'Buzz'
+    }
+    if (!tmpStr.length) tmpStr += num
+    return tmpStr
+  })
+}
+
+var fizzBuzz2 = n => {
   let resultArr = []
   for (let i = 1; i <= n; i++) {
     let tmpStr = ''
